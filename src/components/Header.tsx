961f2cd10d76915842e7226c902d6bb43cefef9a
Caio Padilha <caiopadilha@id.uff.br>
2023-06-01 17:33:16 -0300

    funcionalidades de salvar username no localstorage para mostrar em tela, e função de logout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ export function Header() {
   const username = localStorage.getItem("user");
   return (
     <header className="w-full py-5 bg-gray-700 flex items-center justify-evenly border-b border-gray-600">
-      <Logo />
       <span className="">{`Olá, ${username}!`}</span>
+      <Logo />
       <Logout />
     </header>
   )
